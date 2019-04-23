05043b6cd0e512cf27a75bc2a26de4a1235f5d80
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-23 14:28:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num > 0 do
+    num > 1 ? "#{num} seconds" : "#{num} second"
+    num -= 1
+  end
+end
+
+def countdown_with_sleep(num)
+
 end
