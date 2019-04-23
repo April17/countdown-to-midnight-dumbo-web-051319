e223efae27221446452697e7f8762aabb9d8ee88
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-23 14:35:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,15 @@ def countdown(num)
   while num > 0 do
     puts "#{num} SECOND(S)!"
     num -= 1
-    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
-
+  while num > 0 do
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
